9018e996c6b92a474d08d36a77bc9892e451c71c
Sam PC <mohamadusamahthani@gmail.com>
2025-06-13 23:09:32 +0800

    .

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,27 +1,27 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import { useSearchParams } from "next/navigation"
-import Flashcard from '../components/Flashcard'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import Flashcard from "../../components/Flashcard";
 
-type Card = { char: string; romaji: string  }
+type Card = { char: string; romaji: string };
 
 export default function PracticePage() {
-    const searchParams = useSearchParams()
-    const [cards, setCards] = useState<Card[]>([])
-    const [loading, setLoading] = useState(true)
+	const searchParams = useSearchParams();
+	const [cards, setCards] = useState<Card[]>([]);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const tytpe = searchParams.get('type') || 'hiragana'
-        fetch('/kana.json')
-            .then(res => res.json())
-            .then(data => {
-                setCards(data[tytpe] || [])
-                setLoading(false)
-        })
-    }, [searchParams])
+	useEffect(() => {
+		const tytpe = searchParams.get("type") || "hiragana";
+		fetch("/kana.json")
+			.then((res) => res.json())
+			.then((data) => {
+				setCards(data[tytpe] || []);
+				setLoading(false);
+			});
+	}, [searchParams]);
 
-    if (loading) return <p className="text-center mt-10">Loading...</p>
+	if (loading) return <p className="text-center mt-10">Loading...</p>;
 
-    return <Flashcard cards={cards} />
+	return <Flashcard cards={cards} />;
 }
